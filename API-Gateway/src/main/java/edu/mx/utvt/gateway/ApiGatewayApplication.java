c24ef8a60e059b585d9113b34769ca679e2d1b0c
dyep.frontend <rdylan3871@gmail.com>
2023-04-13 21:03:49 -0500

    xyz

---
+++ API-Gateway/src/main/java/edu/mx/utvt/gateway/ApiGatewayApplication.java
@@ -0,0 +1,21 @@
+package edu.mx.utvt.gateway;
+
+import io.netty.resolver.DefaultAddressResolverGroup;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import reactor.netty.http.client.HttpClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+	public HttpClient httpClient() {
+		return HttpClient.create().resolver(DefaultAddressResolverGroup.INSTANCE);
+	}
+
+}
